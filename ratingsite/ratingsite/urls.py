39db6e45a99e097f853b7502d668d3f23cb46c73
xWilson14 <82909067+xWilson14@users.noreply.github.com>
2025-10-21 20:41:45 +0200

    Update urls.py

--- ratingsite/ratingsite/urls.py
+++ ratingsite/ratingsite/urls.py
@@ -2,11 +2,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("core.urls")),
+    path('admin/', admin.site.urls),
+    # Add your app URLs here if you have any
+    # path('', include('core.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files in production
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
+
+# Serve static files
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
